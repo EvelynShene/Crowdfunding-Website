6e9b3de4672c046358cf2fc6d730cd40d7555b70
EvelynShene <32460422+EvelynShene@users.noreply.github.com>
2017-10-02 12:55:12 -0400

    Add files via upload

---
+++ CourseProject_Crowdfunding/project/modal/add_comment.php
@@ -0,0 +1,29 @@
+<div class="modal fade" id="modal-comment">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                <h4 class="modal-title">Add your comment</h4>
+            </div>
+            <div class="modal-body">
+                <form class="form-horizontal" role="form" id="form-comment">
+                  <div class="form-group">
+                    <label class="control-label col-sm-4" for="name">Comment:</label>
+                    <div class="col-sm-8">
+                      <textarea type="text" id="comms" placeholder="Enter your nickname" class="form-control" required=""> </textarea>
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <div class="col-sm-offset-4 col-sm-4">
+                      <button type="submit" class="btn btn-success col-sm-12">Submit</button>
+                    </div>
+                  </div>
+                 </form>
+            </div>
+            <div class="modal-footer">
+            </div>
+        </div>
+    </div>
+</div>
+
---
+++ CourseProject_Crowdfunding/project/modal/edit_profile.php
@@ -0,0 +1,40 @@
+<div class="modal fade" id="modal-edit">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                <h4 class="modal-title">Edit your profile</h4>
+            </div>
+            <div class="modal-body">
+                <form class="form-horizontal" role="form" id="form-edit">
+                  <div class="form-group">
+                    <label class="control-label col-sm-4" for="name">Nick Name:</label>
+                    <div class="col-sm-8">
+                      <input type="text" class="form-control"  id="name" placeholder="Enter your nickname">
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <label class="control-label col-sm-4" for="hometown">Hometown:</label>
+                    <div class="col-sm-8">
+                      <input type="text" class="form-control"  id="hometown" placeholder="Enter your hometown">
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <label class="control-label col-sm-4" for="ccn">Credit Card Number:</label>
+                    <div class="col-sm-8">
+                      <input type="text" maxlength="16" class="form-control form1"  id="ccn" placeholder="Enter 16-digit credit card number">
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <div class="col-sm-offset-4 col-sm-4">
+                      <button type="submit" class="btn btn-success col-sm-12">Save</button>
+                    </div>
+                  </div>
+                 </form>
+            </div>
+            <div class="modal-footer">
+            </div>
+        </div>
+    </div>
+</div>
+

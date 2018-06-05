54263d8986ce490d0488310675ee28a94b94e33a
Evelyn Shen <32460422+EvelynShene@users.noreply.github.com>
2018-06-04 20:27:22 -0400

    Update and rename leetcode/easy.sql to easy.sql

--- easy.sql
+++ easy.sql
@@ -1,3 +1,5 @@
+# This records some database problems or materials related to MySQL.
+
 595. Big Countries:
 # A country is big if it has an area of bigger than 3 million square km or a population of more than 25 million.
 # Write a SQL solution to output big countries' name, population and area
